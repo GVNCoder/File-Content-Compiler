a4fd549768cef731650f5b9a1b22ab777da1d0a4
DmitrievStanislav <dmitrievsvp@gmail.com>
2021-11-17 11:39:36 +0200

    Created and implemented file content compiler

---
+++ FileContentCompiler/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+using CommandLine;
+
+namespace FileContentCompiler
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<RunOptions>(args)
+                .WithParsed(RunFileContentCompiler);
+        }
+
+        private static void RunFileContentCompiler(RunOptions options)
+        {
+            const string pathGroupName = "path";
+
+            // read all source file content into string
+            var sourceFileContentLines = File.ReadAllLines(options.SourceFilePath);
+            var stringBuilder = new StringBuilder();
+
+            // compile file contents
+            for (var i = 0; i < sourceFileContentLines.Length; i++)
+            {
+                var line = sourceFileContentLines[i];
+                var insertMatch = Regex.Match(line, $"^#include\\s\\\"(?'{pathGroupName}'.*?)\\\"");
+                var matchPath = insertMatch.Success
+                    ? insertMatch.Groups[pathGroupName].Value
+                    : string.Empty;
+
+                // validate file path
+                if (string.IsNullOrWhiteSpace(matchPath) || File.Exists(Path.Combine(Environment.CurrentDirectory, matchPath)) == false)
+                {
+                    stringBuilder.AppendLine(line);
+                }
+                else
+                {
+                    var contentToInsert = File.ReadAllLines(matchPath);
+                    foreach (var content in contentToInsert)
+                    {
+                        stringBuilder.AppendLine(content);
+                    }
+                }
+            }
+
+            // save compiled content into file
+            File.WriteAllText(options.DestinationFilePath, stringBuilder.ToString(), Encoding.UTF8);
+            Console.WriteLine("Done!");
+        }
+    }
+}
---
+++ FileContentCompiler/RunOptions.cs
@@ -0,0 +1,25 @@
+﻿using CommandLine;
+
+namespace FileContentCompiler
+{
+    public class RunOptions
+    {
+        //[Option('b'
+        //    , "base"
+        //    , Required = true
+        //    , HelpText = "Base path for building relative paths.")]
+        //public string BasePath { get; set; }
+
+        [Option('s'
+            , "source"
+            , Required = true
+            , HelpText = "Source file for compilation.")]
+        public string SourceFilePath { get; set; }
+
+        [Option('d'
+            , "destination"
+            , Required = true
+            , HelpText = "The resulting file.")]
+        public string DestinationFilePath { get; set; }
+    }
+}
